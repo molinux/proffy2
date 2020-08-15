b5e2285cf72d84564d2c9af60855f9b7428cc487
Molinux <cybercrowbr@gmail.com>
2020-08-14 23:01:21 -0300

    feat(web): front and back integration with axios
    Day 3: Axios installed state tratment on form

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -11,6 +12,18 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 
 function Landing() {
+  // Como tem o delay da conexão, vamos setar para começar já mostrando 0 conexões
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  // [] vazio pois só vai executar uma vez (durante o carregamento da página)
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +51,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
@@ -10,6 +10,40 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [bio, setBio] = useState('');
+  const [scheduleItems, setScheduleItems ] = useState([
+    { week_day: 0, from: '', to: '' },
+  ])
+
+  const [subject, setSubject] = useState('');
+  const [cost, setCost] = useState('');
+
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day: 0, from: '', to: '' }
+    ]);
+  }
+
+  // Se deixar a 
+  function handleCreateClass(e: FormEvent) {
+    // Como a função está atribuída ao onSubmit do form, por padrão vai dar reload na pagina
+    // vamos "bloquear" o evento default (reload)
+    e.preventDefault();
+    
+    console.log({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost
+    })
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader title="Que incrível que você quer dar aulas."
@@ -17,68 +51,104 @@ function TeacherForm() {
       />
 
       <main>
-        <fieldset>
-          <legend>Seus dados</legend>
-            <Input name="name" label="Nome completo" />
-            <Input name="avatar" label="Avatar" />
-            <Input name="whatsapp" label="WhatsApp" />
-            <Textarea name="bio" label="biografia" />
-        </fieldset>
+        <form onSubmit={handleCreateClass}>
+          <fieldset>
+            <legend>Seus dados</legend>
+              <Input 
+                name="name" 
+                label="Nome completo" 
+                value={name} 
+                onChange={(e) => {setName(e.target.value)}}
+              />
 
-        <fieldset>
-          <legend>Sobre a aula</legend>
-            <Select 
-              name="subject" 
-              label="Matéria"
-              options={[
-                { value: 'Artes', label: 'Artes'},
-                { value: 'Biologia', label: 'Biologia'},
-                { value: 'Matemática', label: 'Matemática'},
-                { value: 'Português', label: 'Português'},
-                { value: 'Química', label: 'Química'},
-                { value: 'História', label: 'História'},
-              ]}
-            />
-            <Input name="cost" label="Custo da sua hora por aula" />
-        </fieldset>
+              <Input 
+                name="avatar" 
+                label="Avatar" 
+                value={avatar} 
+                onChange={(e) => {setAvatar(e.target.value)}}
+              />
 
-        <fieldset>
-          <legend>
-            Horários disponíveis
-            <button type="button">
-              + Novo horário
+              <Input 
+                name="whatsapp" 
+                label="WhatsApp" 
+                value={whatsapp} 
+                onChange={(e) => {setWhatsapp(e.target.value)}}
+              />
+
+              <Textarea 
+                name="bio" 
+                label="Biografia" 
+                value={bio} 
+                onChange={(e) => {setBio(e.target.value)}}
+              />
+          </fieldset>
+
+          <fieldset>
+            <legend>Sobre a aula</legend>
+              <Select 
+                name="subject" 
+                label="Matéria"
+                value={subject}
+                onChange={(e) => {setSubject(e.target.value)}}
+                options={[
+                  { value: 'Artes', label: 'Artes'},
+                  { value: 'Biologia', label: 'Biologia'},
+                  { value: 'Matemática', label: 'Matemática'},
+                  { value: 'Português', label: 'Português'},
+                  { value: 'Química', label: 'Química'},
+                  { value: 'História', label: 'História'},
+                ]}
+              />
+              <Input 
+                name="cost" 
+                label="Custo da sua hora por aula" 
+                value={cost}
+                onChange={(e) => {setCost(e.target.value)}}
+              />
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              Horários disponíveis
+              <button onClick={addNewScheduleItem} type="button">
+                + Novo horário
+              </button>
+            </legend>
+            
+            {scheduleItems.map(scheduleItem => {
+              return (
+                <div key={scheduleItem.week_day} className="schedule-item">
+                  <Select
+                    name="week_day"
+                    label="Dia da semana"
+                    options={[
+                      { value: '0', label: 'Domingo' },
+                      { value: '1', label: 'Segunda-feira' },
+                      { value: '2', label: 'Terça-feira' },
+                      { value: '3', label: 'Quarta-feira' },
+                      { value: '4', label: 'Quinta-feira' },
+                      { value: '5', label: 'Sexta-feira' },
+                      { value: '6', label: 'Sábado' },
+                    ]}
+                  />
+                  <Input name="from" label="Das" type="time" />
+                  <Input name="to" label="Até" type="time" />
+                </div>
+              );
+            })}
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso importante"/>
+              Importante ! <br />
+              Preencha todos os dados
+            </p>
+            <button type="submit">
+              Salvar cadastro
             </button>
-          </legend>
-
-          <div className="schedule-item">
-            <Select
-              name="week_day"
-              label="Dia da semana"
-              options={[
-                { value: '0', label: 'Domingo' },
-                { value: '1', label: 'Segunda-feira' },
-                { value: '2', label: 'Terça-feira' },
-                { value: '3', label: 'Quarta-feira' },
-                { value: '4', label: 'Quinta-feira' },
-                { value: '5', label: 'Sexta-feira' },
-                { value: '6', label: 'Sábado' },
-              ]}
-            />
-            <Input name="from" label="Das" type="time" />
-            <Input name="to" label="Até" type="time" />
-          </div>
-        </fieldset>
-
-        <footer>
-          <p>
-            <img src={warningIcon} alt="Aviso importante"/>
-            Importante ! <br />
-            Preencha todos os dados
-          </p>
-          <button type="button">
-            Salvar cadastro
-          </button>
-        </footer>
+          </footer>
+        </form>
       </main>
     </div>
   )
