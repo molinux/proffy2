46ca46a52f3fd5cba153d6aaffd2396aefec6ce5
Molinux <cybercrowbr@gmail.com>
2020-08-16 20:30:19 -0300

    feat(web): :pill: Web interface finished
    :heavy_check_mark: Web interface has been finished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      users_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/21347783?s=460&u=ffe9da3ab2586c94be5890bd3ff7c4817675cf2a&v=4" alt="Molinux" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Marcus Molinero</strong>
-          <span>Linux</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias opensource.
-            <br />
-            Apaixonado por otimizar o fluxo das empresas através de tecnologias livres.
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 60</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        {/*  
+        https://faq.whatsapp.com/general/chats/how-to-use-click-to-chat/ 
+        TODO: Personalizar mensagem do WhatsApp
+        */}
+        <a 
+          target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
               Entrar em contato
-            </button>
+        </a>
       </footer>
     </article>
   )
